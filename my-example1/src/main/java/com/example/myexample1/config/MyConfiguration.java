d4a9c36a6b7b62db455cc1c4c7612c8def569e87
caiyn <caiyaning@elensdata.com>
2019-09-20 10:54:46 +0800

    init

--- my-example1/src/main/java/com/example/myexample1/config/MyConfiguration.java
+++ my-example1/src/main/java/com/example/myexample1/config/MyConfiguration.java
@@ -3,6 +3,8 @@ package com.example.myexample1.config;
 import org.neo4j.ogm.config.ClasspathConfigurationSource;
 import org.neo4j.ogm.config.ConfigurationSource;
 import org.neo4j.ogm.session.SessionFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
@@ -15,10 +17,12 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement
 public class MyConfiguration {
 
+    private final Logger logger = LoggerFactory.getLogger(MyConfiguration.class);
     @Bean
     public org.neo4j.ogm.config.Configuration configuration() {
         ConfigurationSource properties = new ClasspathConfigurationSource(
                 "db.properties");
+        logger.info("init");
         return new org.neo4j.ogm.config.Configuration.Builder(properties).build();
     }
 
